3ab9f5e5882d5da305eb6a2911f1973733864c69
AjayKhati <ajaysingh0001123@gmail.com>
2025-06-14 23:43:33 +0530

    2nd update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2025 All rights reserved by <a className='text-blue-500' href="https://ajaykhati.netlify.app" target='_blank' rel="noreferrer">AjaySingh</a>
+    </p>
+  </div>
+);
+
+export default Footer;
